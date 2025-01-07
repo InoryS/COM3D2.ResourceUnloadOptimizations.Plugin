a1f6265320e31b132102224ab8beda37320a2182
90135 <101243352+InoryS@users.noreply.github.com>
2025-01-07 19:00:26 +0800

    bump version

--- COM3D2.ResourceUnloadOptimizations.Plugin/ResourceUnloadOptimizations.cs
+++ COM3D2.ResourceUnloadOptimizations.Plugin/ResourceUnloadOptimizations.cs
@@ -21,7 +21,7 @@ namespace COM3D2.ResourceUnloadOptimizations.Plugin
     {
         public const string GUID = "COM3D2.ResourceUnloadOptimizations.Plugin.InoryS";
         public const string PluginName = "COM3D2.ResourceUnloadOptimizations.Plugin";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         private static new ManualLogSource Logger;
 
